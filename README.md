e7e4b6973ceca9251a300aa55f5c05593d77cde8
Theo <theo.faull@westofengland-ca.gov.uk>
2026-05-18 18:09:07 +0100

    first viz

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # weca_data_visualisations
 A public repo to contain non-sensitive data visuals for use in GitHub pages
 
-Each visual should be in their own folder as an .html page for GitHub pages to reference. Scripts should sit within the business area parent folder (eg. `dds` or `economic-briefings`)
+Each visual should be in their own folder as an .html page for GitHub pages to reference. Scripts should sit within the scripts folder.
 
 Data visuals can be made using python `plotly` (`uv pip install plotly`)
 
---
+++ scripts/dds_viz01.py
@@ -0,0 +1,33 @@
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+
+categories = ['Data Quality', 'Integration', 'Skills', 'Governance', 'Technology', 'Culture']
+
+current = [2, 1, 3, 2, 3, 2]
+target = [5, 4, 4, 5, 4, 5]
+
+fig = go.Figure()
+
+
+fig.add_trace(go.Scatterpolar(
+    r=target,
+    theta=categories,
+    fill='toself',
+    name='Target State'
+))
+
+fig.add_trace(go.Scatterpolar(
+    r=current,
+    theta=categories,
+    fill='toself',
+    name='Current State'
+))
+
+fig.update_layout(
+    polar=dict(radialaxis=dict(visible=True, range=[0, 5])),
+    title="Digital & Data Maturity: Current vs Target minimum (2030)"
+)
+
+# Save as a standalone .html file within the visuals folder
+fig.write_html(r"visuals/dds/viz01/plotly_chart.html", include_plotlyjs="cdn")
